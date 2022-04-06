f05a7b9c7a31acf036625f49187045883a1d11ce
ArsenyKorolkovStudent <86719666+ArsyMus@users.noreply.github.com>
2022-04-06 18:41:15 +0300

    Finished

--- src/org/Lab3/Main.java
+++ src/org/Lab3/Main.java
@@ -5,26 +5,52 @@ public class Main {
 
         /* Exercise 1 */
         int a,b,c,d,e,f;
+        int[] array, array1;
 
         /* 1.1. */
-        //oddNumbers();
+        oddNumbers();
 
         /* 1.2. */
-        //secondVivod();
+        secondVivod();
 
         /* 1.3. */
         a = 3;
         b = 9;
         c = 12;
-
-        //sumEqual(a,b,c);
+        sumEqual(a,b,c);
 
         /* 1.4. */
         d = 4;
         e = 6;
         f = 7;
-
         multAll(d,e,f);
+
+        /* 1.5. */
+        array = new int[] {3,5,3,5,3,6};
+        checkArray1(array);
+
+        /* 1.6. */
+        array1 = new int[] {9,5,4,5,0,6};
+        System.out.println(checkArray2(array1));
+    }
+
+    private static boolean checkArray2(int[] array) {
+        boolean ret = false;
+
+        for (int item : array) {
+            if (item == 1 || item == 3){
+                ret = true;
+                return ret;
+            }
+        }
+        return ret;
+    }
+
+    private static void checkArray1(int[] array) {
+        boolean ret;
+
+        ret = array[0] == 3 || array[array.length - 1] == 3;
+        System.out.println(ret);
     }
 
     private static void multAll(int d, int e, int f) {
@@ -34,7 +60,7 @@ public class Main {
         System.out.println("Second number: " + e);
         System.out.println("Third number: " + f);
 
-        ret = e > d && f > e ? true : false;
+        ret = e > d && f > e;
 
         System.out.println("Result: " + ret);
 
@@ -48,7 +74,7 @@ public class Main {
         System.out.println("Second number: " + b);
         System.out.println("Third number: " + c);
 
-        ret = a + b == c ? true : false;
+        ret = a + b == c;
 
         System.out.println("Result: " + ret);
     }
